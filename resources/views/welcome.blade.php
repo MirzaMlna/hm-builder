987dd7844c22dc8eff24080f11ecf8eae7ff39b6
MirzaMlna <mirzamaulana713@gmail.com>
2025-07-22 19:26:12 +0800

    💄 Applying Logo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,8 +11,9 @@
 
 <body class="bg-gray-100 min-h-screen flex items-center justify-center font-sans antialiased">
     <div class="bg-white p-8 rounded-lg shadow-md w-full max-w-md text-center">
-        <x-application-logo class="mx-auto w-20 h-20 mb-6 fill-current text-gray-700" />
-
+        <div class="flex justify-center mb-6">
+            <x-application-logo class="w-20 h-20 fill-current text-gray-700" />
+        </div>
         <h1 class="text-xl font-semibold mb-4 text-gray-800">Selamat Datang di {{ config('app.name') }}</h1>
         <p class="text-sm text-gray-600 mb-6">Silakan login atau daftar untuk melanjutkan.</p>
 
