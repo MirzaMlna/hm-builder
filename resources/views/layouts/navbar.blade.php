72c6ec85e981428d475846dd1d06ff891d886241
MirzaMlna <mirzamaulana713@gmail.com>
2025-08-10 03:54:50 +0800

    💄 Updating Little Bit UI

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -17,6 +17,9 @@
         <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
             {{ __('Dashboard') }}
         </x-responsive-nav-link>
+        <x-responsive-nav-link :href="route('workers.index')" :active="request()->routeIs('workers.index')">
+            {{ __('Tukang') }}
+        </x-responsive-nav-link>
         <x-responsive-nav-link :href="route('profile.edit')" :active="request()->routeIs('profile.edit')">
             {{ __('Profile') }}
         </x-responsive-nav-link>
